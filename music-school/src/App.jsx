d63add4bb4c70a07d1d1e59fae2279ea51a56735
vishwesh129 <nakkalavishwesh@gmail.com>
2023-09-13 15:55:32 +0530

    first commit

---
+++ music-school/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1>Hello from react + vite + Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
