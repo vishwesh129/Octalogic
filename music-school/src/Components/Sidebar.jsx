250a6d4c4ad2be20cb967ca70c77ec145e1fbdc1
vishwesh129 <nakkalavishwesh@gmail.com>
2023-09-14 09:18:42 +0530

    side bar completed

--- music-school/src/Components/Sidebar.jsx
+++ music-school/src/Components/Sidebar.jsx
@@ -17,13 +17,13 @@ export default function Sidebar() {
         <img src={Logo} />
       </div>
       <div style={{ marginTop: "50px", display: "flex", flexDirection: "column", gap:"20px" }}>
-        <button style={{backgroundColor: "#FEDFE1"}} className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button style={{backgroundColor: "#FEDFE1"}} className="flex w-full justify-center rounded-md px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           <div>
             <img src={home} />
             <h1 style={{color: "#901E75"}}>Home</h1>
           </div>
         </button>
-        <button onClick={handleCourses} style={{backgroundColor: "#E5E7EB"}} className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button onClick={handleCourses} style={{backgroundColor: "#E5E7EB"}} className="flex w-full justify-center rounded-md bg-gray-600 px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           <div>
             <img src={courses} />
             <h1 style={{color: "#83858B"}}>Courses</h1>
